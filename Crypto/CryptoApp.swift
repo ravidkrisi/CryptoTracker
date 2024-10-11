ffd2026d6826cc415416c8737ef110489e9a7273
ravidkrisi <ravidkrisi22@gmail.com>
2024-10-11 18:21:52 +0700

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Ravid Krisi on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
