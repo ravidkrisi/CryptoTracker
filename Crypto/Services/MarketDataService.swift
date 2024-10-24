a5f34859a3f4892d7bdc5510af2d83e06edbe814
ravidkrisi <ravidkrisi22@gmail.com>
2024-10-24 11:17:57 +0700

    [feature] added MarketDataService

---
+++ Crypto/Services/MarketDataService.swift
@@ -0,0 +1,31 @@
+//
+//  MarketDataService.swift
+//  Crypto
+//
+//  Created by Ravid Krisi on 24/10/2024.
+//
+
+import Combine
+import Foundation
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
