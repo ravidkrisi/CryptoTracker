98804045621b73972ec86f5326f30a20cdf4fbdf
ravidkrisi <ravidkrisi22@gmail.com>
2024-10-24 11:45:11 +0700

    [feature] added subscriber to marketData on MarketDataService

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,12 +10,7 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-    @Published var statistics: [StatisticsModel] = [
-        StatisticsModel(title: "title", value: "value", precentageChange: 1.0),
-        StatisticsModel(title: "title", value: "value"),
-        StatisticsModel(title: "title", value: "value"),
-        StatisticsModel(title: "title", value: "value", precentageChange: -7.0)
-    ]
+    @Published var statistics: [StatisticsModel] = []
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
@@ -24,7 +19,8 @@ class HomeViewModel: ObservableObject {
     
     private var cancellables = Set<AnyCancellable>()
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     
     init() {
         addSubsricbers()
@@ -33,13 +29,42 @@ class HomeViewModel: ObservableObject {
     func addSubsricbers() {
         // updates allCoins
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] returnedStats in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticsModel] {
+        var stats: [StatisticsModel] = []
+        
+        guard let data = marketDataModel else { return stats }
+        
+        let marketCap = StatisticsModel(title: "Market Cap", value: data.marketCap, precentageChange: data.marketCapChangePercentage24HUsd)
+        
+        let volume = StatisticsModel(title: "24h Volume", value: data.volume)
+        
+        let btcDominance = StatisticsModel(title: "BTC Dominance", value: data.btcDominance)
+        
+        let portfolio = StatisticsModel(title: "Portfolio Value", value: "$0.00", precentageChange: 0)
+        
+        stats.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        return stats
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
