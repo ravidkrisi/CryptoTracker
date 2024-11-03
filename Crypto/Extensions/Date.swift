703cab24b41e3669b379bfbd78444d7ea5b16b36
ravidkrisi <ravidkrisi22@gmail.com>
2024-11-03 19:46:08 +0700

    [feature] added ChartView to DetailView

---
+++ Crypto/Extensions/Date.swift
@@ -0,0 +1,28 @@
+//
+//  Date.swift
+//  Crypto
+//
+//  Created by Ravid Krisi on 03/11/2024.
+//
+
+import Foundation
+
+extension Date {
+    
+    init(coinGeckoString: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let date = formatter.date(from: coinGeckoString) ?? .now
+        self.init(timeInterval: 0, since: date)
+    }
+    
+    private var shortFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+    
+    func asShortDateString() -> String {
+        return shortFormatter.string(from: self)
+    }
+}
