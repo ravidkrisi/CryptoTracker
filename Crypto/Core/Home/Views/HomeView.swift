95c5fe7b1cdaf23a10736ae37072455d7a52c3fb
ravidkrisi <ravidkrisi22@gmail.com>
2024-11-05 09:41:31 +0700

    [feature[ added SettingsView

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
     
     @State private var showPortfolio: Bool = false // animate transition to portfolio screen
     @State private var showPortfolioView: Bool = false // open new sheet
-    
+    @State private var showSettingsView: Bool = false // new sheet
     @State private var selectedCoin: CoinModel? = nil
     @State private var showDetailView: Bool = false
      
@@ -49,6 +49,9 @@ struct HomeView: View {
                     }
                     Spacer(minLength: 0)
                 }
+                .sheet(isPresented: $showSettingsView) {
+                    SettingsView()
+                }
             }
             .navigationDestination(isPresented: $showDetailView, destination: {
                 DetailLoadingView(coin: $selectedCoin)
@@ -71,6 +74,8 @@ extension HomeView {
                 .onTapGesture {
                     if showPortfolio {
                         showPortfolioView.toggle()
+                    } else if !showPortfolio {
+                        showSettingsView.toggle()
                     }
                 }
                 .background(
---
+++ Crypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,81 @@
+//
+//  SettingsView.swift
+//  Crypto
+//
+//  Created by Ravid Krisi on 04/11/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://github.com/ravidkrisi")!
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                infoSection
+                coinGeckoSection
+            }
+            .font(.headline)
+            .tint(.blue)
+            .listStyle(.grouped)
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    xMarkButton
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
+
+extension SettingsView {
+    
+    private var xMarkButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "xmark")
+        }
+    }
+    
+    private var infoSection: some View {
+        Section("Crypto Tracker") {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(.rect(cornerRadius: 20))
+                
+                Text("this app was made following YouTube course.")
+            }
+            .padding(.vertical)
+            Link("check my GitHub", destination: personalURL)
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section("CoinGecko") {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(.rect(cornerRadius: 20))
+                
+                Text("this app was made following YouTube course.")
+            }
+            .padding(.vertical)
+            Link("check coin gecko API", destination: coingeckoURL)
+        }
+    }
+}
